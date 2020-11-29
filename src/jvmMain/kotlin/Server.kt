396b773e4f55a5a8d4817f69818469b314cb1724
lightseba <sdv@knights.ucf.edu>
2020-11-29 01:45:02 -0500

    fix repo

---
+++ src/jvmMain/kotlin/Server.kt
@@ -0,0 +1,66 @@
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.http.*
+import io.ktor.http.content.*
+import io.ktor.request.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.serialization.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import org.litote.kmongo.*
+import org.litote.kmongo.coroutine.*
+import org.litote.kmongo.reactivestreams.KMongo
+import com.mongodb.ConnectionString
+
+// val shoppingList = mutableListOf(
+//     ShoppingListItem("Cucumbers 🥒", 1),
+//     ShoppingListItem("Tomatoes 🍅", 2),
+//     ShoppingListItem("Orange Juice 🍊", 3)
+// )
+
+val client = KMongo.createClient().coroutine
+val database = client.getDatabase("shoppingList")
+val collection = database.getCollection<ShoppingListItem>()
+
+fun main() {
+    embeddedServer(Netty, 9090) {
+        install(ContentNegotiation) {
+            json()
+        }
+        install(CORS) {
+            method(HttpMethod.Get)
+            method(HttpMethod.Post)
+            method(HttpMethod.Delete)
+            anyHost()
+        }
+        install(Compression) {
+            gzip()
+        }
+        routing {
+            route(ShoppingListItem.path) {
+                get {
+                    call.respond(collection.find().toList())
+                }
+                post {
+                    collection.insertOne(call.receive<ShoppingListItem>())
+                    call.respond(HttpStatusCode.OK)
+                }
+                delete("/{id}") {
+                    val id = call.parameters["id"]?.toInt() ?: error("Invalid delete request")
+                    collection.deleteOne(ShoppingListItem::id eq id)
+                    call.respond(HttpStatusCode.OK)
+                }
+            }
+            get("/") {
+                call.respondText(
+                    this::class.java.classLoader.getResource("index.html")!!.readText(),
+                    ContentType.Text.Html
+                )
+            }
+            static("/") {
+                resources("")
+            }
+        }
+    }.start(wait = true)
+}
